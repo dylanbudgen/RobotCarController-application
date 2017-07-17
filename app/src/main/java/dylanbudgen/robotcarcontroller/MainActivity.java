9c2d48b37d81fe725a9be5662a371ae654d7d850
Dylan Budgen <dab45@kent.ac.uk>
2017-07-17 23:52:54 +0100

    Toolbar buttons setup.

--- app/src/main/java/dylanbudgen/robotcarcontroller/MainActivity.java
+++ app/src/main/java/dylanbudgen/robotcarcontroller/MainActivity.java
@@ -9,6 +9,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -31,7 +33,19 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(myToolbar);
 
 
+        // Initiate BL wrapper
+        mBleWrapper = new BleWrapper(this, new BleWrapperUiCallbacks.Null() {
 
+            // Override the methods for Bluetooth
+
+        });
+
+        // Check if BLE is supported by the device
+        if(mBleWrapper.checkBleHardwareAvailable() == false) {
+            Toast.makeText(this, "No BLE compatible hardware detected",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
 
 
 
@@ -41,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void connect(View view) {
+    public void connect() {
 
         // Permissions for fine location called when user wishes to use Bluetooth
         if (Build.VERSION.SDK_INT > 22) {  // Device needs runtime permissions
@@ -68,9 +82,6 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-
-
-
     }
 
     @Override
@@ -91,8 +102,59 @@ public class MainActivity extends AppCompatActivity {
 
 
 
+    // Required to set up toolbar and add buttons from res/menu/menu.xml
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
 
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.action_connect:
+                // Connect button pressed
+                Log.d("DEBUG", "000P Connect button pressed");
+                connect();
+                return true;
+
+            case R.id.action_settings:
+                // Settings button pressed
+                Log.d("DEBUG", "000P Settings button pressed");
+                showAbout();
+                return true;
+
+            case R.id.action_about:
+                // About button pressed
+                Log.d("DEBUG", "000P About button pressed");
+                showSettings();
+                return true;
+
+            default:
+                // Input not recognised, invoke superclass.
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    // Show the about page
+    private void showAbout() {
+        return;
+    }
+
+
+
+
+    // Change the settings
+    private void showSettings() {
+        return;
+    }
 
 
 }
