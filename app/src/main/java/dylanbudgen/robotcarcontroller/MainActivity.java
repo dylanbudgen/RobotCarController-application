c08789ccf838d7687a73972544ac9bc8375b0cf8
Dylan Budgen <dab45@kent.ac.uk>
2017-07-17 22:43:33 +0100

    First commit.

---
+++ app/src/main/java/dylanbudgen/robotcarcontroller/MainActivity.java
@@ -0,0 +1,32 @@
+package dylanbudgen.robotcarcontroller;
+
+import android.bluetooth.BluetoothDevice;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import dylanbudgen.robotcarcontroller.bluetooth.BleWrapper;
+import dylanbudgen.robotcarcontroller.bluetooth.BleWrapperUiCallbacks;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+}
