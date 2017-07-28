5e2c268146e27458c4604170120a55a7a201a449
Dylan Budgen <dab45@kent.ac.uk>
2017-07-28 18:11:34 +0100

    Popup animation added. Warning sounds added. Bug fixes.

--- app/src/main/java/dylanbudgen/robotcarcontroller/SettingsActivity.java
+++ app/src/main/java/dylanbudgen/robotcarcontroller/SettingsActivity.java
@@ -24,7 +24,6 @@ public class SettingsActivity extends AppCompatActivity {
         myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Log.d("DEBUG", "000P First back print");
                 onBackPressed();
             }
         });
