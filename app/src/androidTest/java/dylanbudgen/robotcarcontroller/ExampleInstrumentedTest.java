ae35825e08c8feccf48dbe79031003de13b7a74d
Dylan Budgen <dab45@kent.ac.uk>
2017-08-01 19:06:44 +0100

    Bug fixes - v1.3

--- app/src/androidTest/java/dylanbudgen/robotcarcontroller/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/dylanbudgen/robotcarcontroller/ExampleInstrumentedTest.java
@@ -23,4 +23,12 @@ public class ExampleInstrumentedTest {
 
         assertEquals("dylanbudgen.robotcarcontroller", appContext.getPackageName());
     }
+
+
+    @Test
+    public void test() {
+
+
+
+    }
 }
