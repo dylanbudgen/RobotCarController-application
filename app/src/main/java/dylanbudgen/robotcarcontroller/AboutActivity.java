397d63905e94f47c89aafab21eb9785d7d5152c3
Dylan Budgen <15916056+dylanbudgen@users.noreply.github.com>
2019-07-24 22:38:23 +0100

    Migrated to AndroidX

--- app/src/main/java/dylanbudgen/robotcarcontroller/AboutActivity.java
+++ app/src/main/java/dylanbudgen/robotcarcontroller/AboutActivity.java
@@ -2,10 +2,10 @@ package ***REMOVED***robotcarcontroller;
 
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
--- app/src/main/java/dylanbudgen/robotcarcontroller/SettingsActivity.java
+++ app/src/main/java/dylanbudgen/robotcarcontroller/SettingsActivity.java
@@ -1,10 +1,10 @@
 package ***REMOVED***robotcarcontroller;
 
 import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
+import androidx.appcompat.widget.Toolbar;
+
 import android.view.Menu;
 import android.view.View;
 
